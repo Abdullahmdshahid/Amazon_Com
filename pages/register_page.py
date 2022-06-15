e67ef364c6cbfd63205b2200f47400eee74fa0cb
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2022-06-15 23:46:28 +0600

    update register page

--- pages/register_page.py
+++ pages/register_page.py
@@ -1,7 +1,4 @@
-from selenium.webdriver import Keys
-
 from pages.base_page import BasePage
-from files.sample_data import SampleData
 from utils.locators import *
 
 
@@ -11,18 +8,18 @@ class RegisterPage(BasePage):
         super().__init__(chrome_webdriver)
         self.locator = RegisterPageLocator
 
-    def create_new_account(self):
-        name = self.chrome_webdriver.find_element(*self.locator.USER_NAME_FIELD)
-        name.send_keys(SampleData.name)
+    def create_new_account(self, name, email, password, confirmpassword):
+        name_text_box = self.chrome_webdriver.find_element(*self.locator.USER_NAME_FIELD)
+        name_text_box.send_keys(name)
 
-        email = self.chrome_webdriver.find_element(*self.locator.EMAIL_FIELD)
-        email.send_keys(SampleData.email)
+        email_text_box = self.chrome_webdriver.find_element(*self.locator.EMAIL_FIELD)
+        email_text_box.send_keys(email)
 
-        password = self.chrome_webdriver.find_element(*self.locator.PASSWORD_FIELD)
-        password.send_keys(SampleData.password)
+        password_text_box = self.chrome_webdriver.find_element(*self.locator.PASSWORD_FIELD)
+        password_text_box.send_keys(password)
 
         re_password = self.chrome_webdriver.find_element(*self.locator.RE_ENTER_PASSWORD)
-        re_password.send_keys(SampleData.password)
+        re_password.send_keys(confirmpassword)
 
         button = self.chrome_webdriver.find_element(*self.locator.BUTTON)
         button.click()
