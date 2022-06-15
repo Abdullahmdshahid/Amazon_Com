c99e07e63ac1604a931948c19768d70298dcd953
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2022-06-15 23:02:32 +0600

    update conftest file, change driver file path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-
+from selenium.webdriver.chrome.service import Service
+import os
 
 @pytest.fixture(scope='class')
 def main(request):
-    chrome_webdriver = webdriver.Chrome(executable_path=r"F:\Amazon\browser\chromedriver.exe")
+    chrome_service = Service(os.getcwd() + r"\browser\chromedriver.exe")
+    chrome_webdriver = webdriver.Chrome(service=chrome_service)
     chrome_webdriver.get("https://www.amazon.com/")
     chrome_webdriver.maximize_window()
-    # action = ActionChains(chrome_webdriver)
     request.cls.chrome_webdriver = chrome_webdriver
+
     yield
     chrome_webdriver.close()
