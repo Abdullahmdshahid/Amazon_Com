316f818e0eb456b2106e07bdb91dc79f4ae01f52
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2022-06-17 12:29:19 +0600

    test 2_2 is for login with email and password

--- tests/test_2_2.py
+++ tests/test_2_2.py
@@ -1,19 +1,15 @@
-import pytest
+from  tests.base_test import BaseTestClass
 from pages.home_page import HomePage
 from pages.sign_in_page import SignInPage
+from files.sample_data import LoginData
 
 
-@pytest.mark.usefixtures("main")
-class Test(object):
+class Test(BaseTestClass):
     def test_tc_2_2(self):
         home_page = HomePage(self.chrome_webdriver)
         home_page.click_login_button()
 
         sin_in_page = SignInPage(self.chrome_webdriver)
-        sin_in_page.click_continue_button_with_email()
-        sin_in_page.click_continue_button_with_password()
-        home_page.hover_profile()
-        # user_name = home_page.get_profile_name()
-        # home_page.click_log_out()
-        print(home_page.get_profile_name())
-        # assert "aaa" == user_name
+        sin_in_page.click_continue_button_with_email(LoginData.EMAIL)
+        sin_in_page.click_continue_button_with_password(LoginData.PASSWORD)
+        assert "approve" in sin_in_page.get_approve_text()
